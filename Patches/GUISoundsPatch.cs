e86939104552492f1265c45ea1da6e118e58cdd0
Sergey Syrtsev <syrtsevser@gmail.com>
2025-06-15 16:55:46 +0300

    Add project files.

---
+++ Patches/GUISoundsPatch.cs
@@ -0,0 +1,47 @@
+﻿using EFT.UI;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using DG.Tweening;
+using UnityEngine;
+
+// ReSharper disable InconsistentNaming
+namespace Mugnum.TarkovMods.MusicPlaysInHideout.Patches
+{
+	/// <summary>
+	/// Patch for <see cref="GUISounds"/> class.
+	/// </summary>
+	internal class GUISoundsPatch : ModulePatch
+	{
+		/// <summary>
+		/// Returns method to override.
+		/// </summary>
+		/// <returns> Target method. </returns>
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(GUISounds).GetMethod(nameof(GUISounds.method_6));
+		}
+
+		/// <summary>
+		/// Prefix patch.
+		/// </summary>
+		/// <param name="isActive"> Is menu music active. False indicates we're entering hideout. </param>
+		/// <param name="__instance"> Context instance. </param>
+		/// <param name="___audioSource_3"> "audioSource_3" private field (music category). </param>
+		/// <returns> Flag indicating the need to execute original method. </returns>
+		[PatchPrefix]
+		private static bool PatchPrefix(bool isActive, GUISounds __instance, AudioSource ___audioSource_3)
+		{
+			const bool IsNeedToExecuteOriginalMethod = false;
+			const float BaseVolume = 1f;
+			var transitionDurationInSec = Mathf.Clamp(Plugin.TransitionTimeInSec.Value, 0f, 10f);
+			var newVolume = isActive
+				? BaseVolume
+				: BaseVolume * Plugin.RelativeVolume.Value;
+
+			__instance.BackgroundMusicActive = true;
+			___audioSource_3.DOFade(newVolume, transitionDurationInSec);
+
+			return IsNeedToExecuteOriginalMethod;
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using JetBrains.Annotations;
+using Mugnum.TarkovMods.MusicPlaysInHideout.Extensions;
+using Mugnum.TarkovMods.MusicPlaysInHideout.Patches;
+
+namespace Mugnum.TarkovMods.MusicPlaysInHideout
+{
+	/// <summary>
+	/// Music plays in hideout mod.
+	/// </summary>
+	[BepInPlugin("Mugnum.MusicPlaysInHideout", "Music plays in hideout", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+		/// <summary>
+		/// Relative volume setting.
+		/// </summary>
+		internal static ConfigEntry<float> RelativeVolume;
+
+		/// <summary>
+		/// Transition time (in seconds) setting.
+		/// </summary>
+		internal static ConfigEntry<float> TransitionTimeInSec;
+
+		/// <summary>
+		/// Initializes mod.
+		/// </summary>
+		[UsedImplicitly]
+		internal void Awake()
+		{
+			InitializeConfig();
+			new GUISoundsPatch().Enable();
+		}
+
+		/// <summary>
+		/// Initializes config.
+		/// </summary>
+		private void InitializeConfig()
+		{
+			const string CommonSection = "Volume";
+			var percentageAttributes = new ConfigurationManagerAttributes
+			{
+				ShowRangeAsPercent = true
+			};
+
+			RelativeVolume = Config.Bind(CommonSection, "Relative hideout volume", 1f,
+				new ConfigDescription("Hideout music volume, relative to main menu volume.",
+					new AcceptableValueRange<float>(0f, 1f),
+					percentageAttributes));
+
+			TransitionTimeInSec = Config.Bind(CommonSection, "Transition time", 2f,
+				new ConfigDescription("Music volume fade time (in seconds).",
+					new AcceptableValueRange<float>(0f, 10f)));
+		}
+	}
+}
